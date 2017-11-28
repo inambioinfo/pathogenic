b31cfdb2d64a2e1f5c47d0c9c93a656c5a75cb89
Jing <logust@yahoo.com>
2017-11-28 17:12:30 +0000

    added unfinished parse_vcfs

--- pathogenic.py
+++ pathogenic.py
@@ -6,46 +6,102 @@ all variants as a background distribution
 python3
 '''
 import sys
+import re
 import subprocess
 sys.path.append('../gnomad')
 import os
+from collections import defaultdict
+import pandas as pd
 
-def get_vcfs(gnomad_path, grange):
+def split_iter(string):
+    '''
+    iter through string separated by \n
+    '''
+    return (x.group(0) for x in re.finditer(r"[^\n]+",string))
+
+def get_vcfs(gnomad_path, fasta_ref, grange):
     '''
     return a generator, first exome vcf, second gnome vcf
     '''
-    # first get exomes
-    exome_file = os.path.join(
-            gnomad_path,
-            'exomes',
-            'vcf',
-            'gnomad.exomes.r2.0.1.sites.vcf.gz'
+    # define the pipelines
+    def pl(f):
+        ps1 = subprocess.Popen((
+            'tabix',
+            '-h',
+            f,
+            grange
+            ),
+            stdout = subprocess.PIPE
             )
-    result_exome = subprocess.check_output([
-        'tabix',
-        exome_file,
-        grange
-        ]).decode('utf8')
-    
-    # now genome
+        ps2 = subprocess.Popen((
+            'bcftools',
+            'norm',
+            '-Ou',
+            '-m',
+            '-any'),
+            stdin = ps1.stdout,
+            stdout = subprocess.PIPE
+            )
+        xome = subprocess.check_output((
+            'bcftools',
+            'norm',
+            '-Ov',
+            '-f',
+            fasta_ref,
+            ),
+            stdin = ps2.stdout
+            ).decode('utf8')
+        ps1.wait()
+        ps2.wait()
+        return xome
+    # get files
     chrom = grange.split(':')[0]
-    genome_file = os.path.join(
-            gnomad_path,
-            'genomes',
-            'vcf',
-            'gnomad.genomes.r2.0.1.sites.{}.vcf.gz'.format(chrom),
-            )
-    return (
-            subprocess.check_output([
-                'tabix',
-                f,
-                grange
-                ]).decode('utf8')
-            for f in (exome_file,genome_file)
-            )
+    files = {
+            'exomes':os.path.join(
+                gnomad_path,
+                'exomes',
+                'vcf',
+                'gnomad.exomes.r2.0.1.sites.vcf.gz'
+                ),
+            'genomes':os.path.join(
+                gnomad_path,
+                'genomes',
+                'vcf',
+                'gnomad.genomes.r2.0.1.sites.{}.vcf.gz'.format(chrom),
+                )
+    }
+    
+    return (pl(f) for f in files.values())
+
+def parse_vcfs(vcf_generator):
+    '''
+    return a pd.DataFrame, with variant ids on the index column
+    columns:
+    id(dbsnp id), filter, consequence(on main transcript),  ac, af, an, hom
+    afr/amr/eas/fin/nfe/oth/sas (ac,an,af,hom), 
+    male_ac/an/hom, female_ac/an/hom, 
+
+    ignore asj for the time being as it is missing in exome
+    '''
+    vcf_list = defaultdict(dict)
+    for gg in vcf_generator:
+        header = []
+        for row in split_iter(gg):
+            # skip comments and get header
+            if row.startswith('##'): continue
+            row = row.split('\t')
+            if row[0].startswith('#'):
+                header = row
+                print(header)
+                continue
+            v_id = '-'.join([row[0],row[1],row[3],row[4]])
+            for field in ('filter','id'):
+                vcf_list[v_id][field] = row[header.index(field.upper())]
+    print(vcf_list)
 
 
 if __name__ == '__main__':
     gnomad_path = '/media/jing/18117A5842B23232/db/gnomAD/release-170228'
     grange = '1:94458393-94586688'
     clinvar_file = '/media/jing/18117A5842B23232/db/clinvar/clinvar_20171029.vcf.gz'
+    fasta_ref = '/media/jing/18117A5842B23232/db/human_g1k_v37.fasta'
--- tests/test_pathogenic.py
+++ tests/test_pathogenic.py
@@ -6,17 +6,23 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.gnomad_path = '/media/jing/18117A5842B23232/db/gnomAD/'+\
             'release-170228'
+        self.fasta_ref = '/media/jing/18117A5842B23232/db/human_g1k_v37.fasta'
 
     def tearDown(self):
         pass
 
     def test_get_vcfs(self):
         grange = '1:94484000-94484001'
-        result = list(get_vcfs(self.gnomad_path,grange))
+        result = list(get_vcfs(self.gnomad_path,self.fasta_ref,grange))
         self.assertEqual(result[1].split('\t')[2],'rs778234759')
         grange = '1:94461665-94461666'
-        result = list(get_vcfs(self.gnomad_path,grange))
+        result = list(get_vcfs(self.gnomad_path,self.fasta_ref,grange))
         self.assertEqual(result[0].split('\t')[2],'rs761134287')
+    def test_parse_vcfs(self):
+        grange = '1:94461636-94461666'
+        vcfs = get_vcfs(self.gnomad_path,self.fasta_ref,grange)
+        result = parse_vcfs(vcfs)
+        print(result)
 
 if __name__ == '__main__':
     unittest.main()
